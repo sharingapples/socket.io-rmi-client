2b943408c8f6cd9b5d1470c22a0137ac7d6ef071
Ranjan Shrestha <ranjan@sharingapples.com>
2016-05-23 19:45:35 +0545

    exported the required components

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 'use strict';
+
+module.exports = {
+  ClientInterface: require('./lib/client/ClientInterface'),
+  RemoteListener: require('./lib/client/RemoteListener'),
+  ServerInstance: require('./lib/server/ServerInstance'),
+};
